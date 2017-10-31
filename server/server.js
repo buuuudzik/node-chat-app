71302b48fa8598bfd11ef69f6445c197466e57e9
buuuudzik <slawomir.budzynski@gmail.com>
2017-10-31 16:22:48 +0100

    Emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -17,8 +17,11 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log('New message', message);
     
-    message.createdAt = new Date().getTime();
-    socket.emit('newMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
   
   socket.on('disconnect', () => {

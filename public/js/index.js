9313fdd071632451cfd0a64397408bb46914b8e1
buuuudzik <slawomir.budzynski@gmail.com>
2017-10-31 20:22:00 +0100

    Added simple geolocalisation sharing

--- public/js/index.js
+++ public/js/index.js
@@ -24,3 +24,18 @@ $('#message-form').on('submit', function(e) {
     
   });
 });
+
+
+var locationButton = $('#send-location');
+
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.')  
+  };
+  
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position);
+  }, function() {
+    alert('Unable to fetch location.');
+  });
+});

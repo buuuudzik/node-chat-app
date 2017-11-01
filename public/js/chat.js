3f74744fae3ba28c9558d17e3f1e17d7e07d1363
buuuudzik <slawomir.budzynski@gmail.com>
2017-11-01 16:22:50 +0100

    Add joining to rooms and sending welcome message by admin to apropriate groups

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,6 +16,19 @@ function scrollToBottom() {
   }
 }
 
+socket.on('connect', () => {
+  var params = $.deparam(window.location.search);
+  
+  socket.emit('join', params, (err) => {
+    if (err) {
+      alert(err);
+      window.location.href = '/';
+    } else {
+      console.log('No error');
+    };
+  });
+});
+
 socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const http = require('http');
 var express = require('express');
 var socketIO = require('socket.io');
 const {generateMessage, generateLocationMessage} = require('./utils/message');
+const {isRealString} = require('./utils/validation.js');
 
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000;
@@ -13,10 +14,24 @@ var io = socketIO(server);
 app.use(express.static(publicPath)); // map /public on main path
 
 io.on('connection', (socket) => {
-  console.log('New user connection');
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  console.log('New user connection');  
   
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  socket.on('join', (params, callback) => {
+    if (!isRealString(params.name) || !isRealString(params.room)) {
+      callback('Name and room name are required.');
+    };
+    
+    socket.join(params.room);
+    // socket.leave('The Office Fans);
+    
+    // io.emit -> io.to('The Office Fans)
+    // socket.broadcast.emit -> socket.broadcast.to('The Office Fans).emit
+    // socket.emit
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
+    callback();
+  });
+  
   
   socket.on('createMessage', (message, callback) => {
     console.log('New message', message);

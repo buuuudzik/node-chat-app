4a4eefbd552d35225b9bb2d21ff612b90be4474a
buuuudzik <slawomir.budzynski@gmail.com>
2017-11-01 23:15:30 +0100

    Add apropriate user name on create message and location message

--- public/js/chat.js
+++ public/js/chat.js
@@ -77,7 +77,6 @@ $('#message-form').on('submit', function(e) {
   e.preventDefault();
   
   socket.emit('createMessage', {
-    from: 'User',
     text: messageTextBox.val()
   }, function() {
     messageTextBox.val('');
